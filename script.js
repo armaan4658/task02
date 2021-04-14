d157307a04266be2871315c6492f54e9dbf75586
armaan4658 <80997749+armaan4658@users.noreply.github.com>
2021-04-14 19:11:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ req.open('GET','https://restcountries.eu/rest/v2/all',true);
 req.send();
 //loading the function
 req.onload=function(){
+    //converting the data into JSON
     var data = JSON.parse(this.response);
+    console.log("Countries :");
+    //using for loop to print out the country name's
     for(let i = 0; i < 250; i++){
         console.log(data[i].name);
     }
